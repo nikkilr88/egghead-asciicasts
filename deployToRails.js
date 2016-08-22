2d3e4fb29d522ac97320652ae90808981220ca0a
ijones16 <ian@egghead.io>
2016-08-22 13:00:43 -0700

    #230 script automates extracting and deploying ET's

---
+++ deployToRails.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const prompt = require('cli-prompt');
+const axios  = require('axios');
+
+const AUTH_TOKEN = '2977fdfd-9b2f-4948-9960-04e8507957e6';
+axios.defaults.headers.common['AuthToken'] = AUTH_TOKEN;
+
+prompt('Deploy to rails? (y/n) ', function (val) {
+    const deploy = val;
+
+    if (deploy === 'y' || deploy === 'Y' || deploy === 'yes'){
+        prompt('What domain would you like to send these to? ', (val) => {
+            const domain = val;
+            prompt('This will deploy enhancedTranscripts.json, are you sure? (y/n) ', (val) => {
+                if (val === 'y' || val === 'Y' || val === 'yes'){
+                    const extracted_enhanced_transcripts = JSON.parse(fs.readFileSync('enhancedTranscripts.json').toString());
+                    extracted_enhanced_transcripts.forEach((et, index) => {
+                       axios.put(`http://${domain}/api/v1/lessons/${et.lesson_slug}`, {
+                           enhanced_transcript: et.enhanced_transcript
+                       })
+                    });
+                    //axios.put(`http://${domain}/api/v1/lessons/${}`, {
+                    //    enhanced_transcripts: extracted_enhanced_transcripts
+                    //})
+                    //.then(function (response) {
+                    //    //console.log(response);
+                    //})
+                    //.catch(function (error) {
+                    //    console.log(error)
+                    //    console.log('error');
+                    //});
+                } else {
+                    console.log('Come back soon!');
+                }
+            })
+
+        })
+    } else {
+        console.log('Have a good day!')
+    }
+}, function (err) {
+    console.error('unable to read answer: ' + err);
+});
---
+++ extractEnhancedTranscripts.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const prompt = require('prompt');
+
+// Start of script
+//
+// Ask user what course they want to extract
+// Assumes that the lessons are held in a /dir/lessons subfolder
+prompt.start();
+prompt.get(['directory'], function (err, result) {
+    //
+    // Log the results.
+    //
+    console.log('  directory: ' + result.directory);
+    console.log("extracting...");
+    const extractedTranscripts = fs.readdirSync(`${result.directory}/lessons`)
+        .reduce((acc, curr) => {
+            // Use underscores to match rails api
+            const lesson_slug = curr.slice(0, -3);
+            const enhanced_transcript = fs.readFileSync(`${result.directory}/lessons/${curr}`).toString();
+            acc.push({lesson_slug, enhanced_transcript});
+            return acc
+        }, []);
+    fs.writeFileSync('enhancedTranscripts.json', JSON.stringify(extractedTranscripts))
+    console.log('done')
+});
+
