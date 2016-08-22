b845cce9dd5d59ab5da4d59f64f4b00dc9e2687b
ijones16 <ian@egghead.io>
2016-08-22 16:43:56 -0700

    need to specify the protocol (http or https)

--- deployToRails.js
+++ deployToRails.js
@@ -9,26 +9,16 @@ prompt('Deploy to rails? (y/n) ', function (val) {
     const deploy = val;
 
     if (deploy === 'y' || deploy === 'Y' || deploy === 'yes'){
-        prompt('What domain would you like to send these to? ', (val) => {
+        prompt('What domain would you like to send these to? (specify http or https) ', (val) => {
             const domain = val;
             prompt('This will deploy enhancedTranscripts.json, are you sure? (y/n) ', (val) => {
                 if (val === 'y' || val === 'Y' || val === 'yes'){
                     const extracted_enhanced_transcripts = JSON.parse(fs.readFileSync('enhancedTranscripts.json').toString());
                     extracted_enhanced_transcripts.forEach((et, index) => {
-                       axios.put(`http://${domain}/api/v1/lessons/${et.lesson_slug}`, {
+                       axios.put(`${domain}/api/v1/lessons/${et.lesson_slug}`, {
                            enhanced_transcript: et.enhanced_transcript
                        })
                     });
-                    //axios.put(`http://${domain}/api/v1/lessons/${}`, {
-                    //    enhanced_transcripts: extracted_enhanced_transcripts
-                    //})
-                    //.then(function (response) {
-                    //    //console.log(response);
-                    //})
-                    //.catch(function (error) {
-                    //    console.log(error)
-                    //    console.log('error');
-                    //});
                 } else {
                     console.log('Come back soon!');
                 }
