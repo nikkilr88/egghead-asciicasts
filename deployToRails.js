5c2fa9c57cec319d702af03912c171f24e86c274
Creeland Provinsal <Cree@Creelands-MacBook-Air.local>
2016-08-30 13:41:27 -0700

    merge upstream

---
+++ deployToRails.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const prompt = require('cli-prompt');
+const axios  = require('axios');
+
+const AUTH_TOKEN = '2977fdfd-9b2f-4948-9960-04e8507957e6';
+axios.defaults.headers.common['AuthToken'] = AUTH_TOKEN;
+
+prompt('Deploy to rails? (y/n) ', function (val) {
+    const deploy = val;
+
+    if (deploy === 'y' || deploy === 'Y' || deploy === 'yes'){
+        prompt('What domain would you like to send these to? (specify http or https) ', (val) => {
+            const domain = val;
+            prompt('This will deploy enhancedTranscripts.json, are you sure? (y/n) ', (val) => {
+                if (val === 'y' || val === 'Y' || val === 'yes'){
+                    const extracted_enhanced_transcripts = JSON.parse(fs.readFileSync('enhancedTranscripts.json').toString());
+                    extracted_enhanced_transcripts.forEach((et, index) => {
+                       axios.put(`${domain}/api/v1/lessons/${et.lesson_slug}`, {
+                           enhanced_transcript: et.enhanced_transcript
+                       })
+                    });
+                } else {
+                    console.log('Come back soon!');
+                }
+            })
+
+        })
+    } else {
+        console.log('Have a good day!')
+    }
+}, function (err) {
+    console.error('unable to read answer: ' + err);
+});
